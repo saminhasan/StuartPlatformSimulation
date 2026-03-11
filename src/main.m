b71d3e5bae6a42bd022763c17c751fd163649352
saminhasan <mhsamin@mail.ubc.ca>
2026-03-11 10:01:55 -0700

    quick sync

--- src/main.m
+++ src/main.m
@@ -2,7 +2,7 @@ clc; close all; clear all; %#ok<CLALL>
 params;
 sp = StuartPlatform(r, n, rB, dB, rP, dP);
 modes = {'sin', 'syn', 'cam', 'imu', 'mix','c1'};
-mode = modes{6};
+mode = modes{1};
 trajectoryA = genTrajectory(mode, 10);
 trajectoryA(:,6) = trajectoryA(:,6) - deg2rad(6);
 plotTrajectory(trajectoryA);
@@ -17,8 +17,8 @@ trajectoryB = window(trajectoryB, 2.0, fs);
 jointAngles = sp.move(trajectoryB);
 plotMotorAngles(jointAngles);
 % 
-simOut = sim("SPPD.slx");
+simOut = sim("SP.slx");
 plotSimResults(simOut, trajectoryB,trajectoryA, sp,rAB_body);
 % % 
-% % mData = jointAngles(:, 2:7);
-% % writematrix(mData, "imuTraj.csv")
+% mData = jointAngles(:, 2:7);
+% writematrix(mData, "c1Traj.csv")

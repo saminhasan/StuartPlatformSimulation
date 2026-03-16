54028f1c3ec0d5f63e397397dfc304bed50bf362
Muhammad Samin Hasan <saminhasan@iut-dhaka.edu>
2026-03-16 15:04:54 -0700

    quick sync

--- src/csvGen.m
+++ src/csvGen.m
@@ -3,7 +3,9 @@ params;
 sp = StuartPlatform(r, n, rB, dB, rP, dP);
 modes = {'sin', 'syn', 'cam', 'imu', 'mix','c1'};
 mode = modes{1};
-trajectoryA = genTrajectory(mode, 10);
+% trajectoryA = genTrajectory(mode, 10);
+trajectoryA = genPoseSynthetic(25);
+
 plotTrajectory(trajectoryA);
 time = trajectoryA(:,1);
 tf = time(end);
@@ -11,4 +13,4 @@ trajectoryA = window(trajectoryA, 2.0, fs);
 jointAngles = sp.move(trajectoryA);
 plotMotorAngles(jointAngles);
 mData = jointAngles(:, 2:7);
-writematrix(mData, "RzTraj.csv")
+writematrix(mData, "zSynthTraj.csv")
--- src/genPoseSynthetic.m
+++ src/genPoseSynthetic.m
@@ -30,9 +30,9 @@ function trajectory = genPoseSynthetic(n)
     y = 0.05  * sin(2 * pi * f_trajectory * time)*0; % y (horizontal component, left side right side)
     z = s - mean(s);% z (vertical component, up down)
 
-    Rx = deg2rad(3.0)  *  sin(2 * pi * f_trajectory/2 * time+ phi0 -pi/2); % roll
-    Ry = deg2rad(2.5) * sin(2 * pi *  f_trajectory * time+ phi0 -pi/2) + deg2rad(10); % pitch
-    Rz = deg2rad(15.0)  *  sin(2 * pi * f_trajectory/2 * time + phi0 -pi/2); % yaw
+    Rx = deg2rad(3.0)  *  sin(2 * pi * f_trajectory/2 * time+ phi0 -pi/2)*0; % roll
+    Ry = (deg2rad(2.5) * sin(2 * pi *  f_trajectory * time+ phi0 -pi/2) + deg2rad(10))*0; % pitch
+    Rz = deg2rad(15.0)  *  sin(2 * pi * f_trajectory/2 * time + phi0 -pi/2)*0; % yaw
 
     trajectory = [time, x, y, z, Rx, Ry, Rz];
 end
